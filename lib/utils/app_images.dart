1b6747a72de0c53c10830b64b9ff9b2009028b64
Myproject <mohamedAlaa-CS>
2024-02-22 11:59:58 +0200

    generate assets

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,52 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvater1
+  /// assets/images/avater_1.svg
+  static const String imagesAvater1 = "assets/images/avater_1.svg";
+
+  /// Assets for imagesAvater2
+  /// assets/images/avater_2.svg
+  static const String imagesAvater2 = "assets/images/avater_2.svg";
+
+  /// Assets for imagesBackgroundCart
+  /// assets/images/background_cart.svg
+  static const String imagesBackgroundCart = "assets/images/background_cart.svg";
+
+  /// Assets for imagesBalance
+  /// assets/images/balance.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesDashboard
+  /// assets/images/dashboard.svg
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesInvestmantce
+  /// assets/images/investmantce.svg
+  static const String imagesInvestmantce = "assets/images/investmantce.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesMyTrancation
+  /// assets/images/my_trancation.svg
+  static const String imagesMyTrancation = "assets/images/my_trancation.svg";
+
+  /// Assets for imagesSettings
+  /// assets/images/settings.svg
+  static const String imagesSettings = "assets/images/settings.svg";
+
+  /// Assets for imagesStatices
+  /// assets/images/statices.svg
+  static const String imagesStatices = "assets/images/statices.svg";
+
+  /// Assets for imagesWalletAccount
+  /// assets/images/wallet_account.svg
+  static const String imagesWalletAccount = "assets/images/wallet_account.svg";
+}
+
